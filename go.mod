6f7967a3a258401ebd2769796cdff087e6a73b45
Hasbi Shuhada <hasbishuhada123@gmail.com>
2022-12-12 00:41:46 +0700

    commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/justhasbi/learn_go_mod
+
+go 1.18
+
+require github.com/justhasbi/go_sample_module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_sample_module "github.com/justhasbi/go_sample_module"
+)
+
+func main() {
+	fmt.Println(go_sample_module.greeting())
+}
